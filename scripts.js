9eee00f42bf1d99d0a3eef322c76655b35e8a765
Leonardo Peres <leonardofperes96@gmail.com>
2022-09-01 14:49:18 -0300

    Project calculator setup, created index.html and respective styles, selected all the elements necessary to do the project

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const clearButton = document.querySelector('[data-clear]')
+const previousOperand = document.querySelector('[data-previous-operand]')
+const currentOperand = document.querySelector('[data-current-operand]')
+const deleteButton = document.querySelector('[data-delete]')
+const operatorButton = document.querySelectorAll('[data-operator]')
+const equalButton = document.querySelector('[data-equal]')
+const numbersButton = document.querySelectorAll('[data-number]')
+
